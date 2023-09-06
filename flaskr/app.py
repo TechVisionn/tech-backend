b132c133fbb8ae0b3f960f43ab42a43db8b99aad
Zaion Arruda <ZARRUDA@ball.com>
2023-09-05 21:01:03 -0300

    7: feat-initial config db and migration

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flaskr.db import config_sql_alchemy, db_instance
+from flaskr.migrate import load_migrate
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Visiona Start here!</p>"
+# Configurar SQLAlchemy
+config_sql_alchemy(app)
+
+# Load Flask Migrate
+migrate = load_migrate(db_instance, app)
+
+if __name__ == '__main__':
+    app.run()
