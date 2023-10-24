f1d3f4800b983444c5e4a26dda16444d0160a50e
ZaionKun <gomeszaion@gmail.com>
2023-10-23 22:38:20 -0300

    feat-validation options terms

--- flaskr/resources/token.py
+++ flaskr/resources/token.py
@@ -7,7 +7,7 @@ from flask_jwt_extended import (
     jwt_required,
 )
 from flask_restful import Resource
-
+from datetime import datetime
 from flaskr.db.mongo_serve import db_instance
 from flaskr.security import ACCESS_EXPIRES
 
@@ -17,12 +17,15 @@ class TokenResource(Resource):
         super().__init__()
         self.token_instance = db_instance.token
         self.user_instance = db_instance.user
+        self.user_history = db_instance.history
         self.term_instance = db_instance.Term
 
     def post(self):
         _user = request.json.get("_user")
         _pwd = request.json.get("_pwd")
         _term = request.json.get("_term")
+        _term_option_one = request.json.get("_option_one")
+        _term_option_second = request.json.get("_option_second")
 
         user = self.user_instance.find_one({"user": _user, "pwd": _pwd})
         if user is None:
@@ -30,18 +33,54 @@ class TokenResource(Resource):
 
         latest_term = self.term_instance.find_one(sort=[("version", -1)])
         if latest_term != user["term"]["version"] or not latest_term:
-            if _term is False and user["term"]["version"] is None:
-                self.user_instance.delete_one({"user": _user, "pwd": _pwd})
-                return make_response({"message": "User is deleted"})
-            elif _term is False and user["term"]["version"] != None:
+            # user does not participate in the application
+            if _term is False:
+                if user["term"]["version"] is None:
+                    self.user_instance.delete_one({"user": _user, "pwd": _pwd})
+                    return make_response({"message": "User is deleted"})
+                elif user["term"]["version"] != None:
+                    self.user_instance.delete_one({"user": _user, "pwd": _pwd})
+                    self.user_history.update_many({"$unset": {"user": _user, "pwd": _pwd}})
+            # user must accept the terms
+            elif (
+                _term is None
+                and user["term"]["version"] != None
+                or user["term"]["parameters"]["option_one"] != None
+                or user["term"]["parameters"]["option_one"] != None
+            ):
                 return make_response({"message": "User needs to update terms"})
-            #parei aqui
+
             else:
+                _date_now = datetime.today().strftime("%Y-%m-%d")
                 self.user_instance.update_one(
                     {"_id": user["_id"]},
-                    {"$set": {"version_term": latest_term}},
+                    {
+                        "$set": {
+                            "term": {
+                                "version": latest_term,
+                                "date_accepted": _date_now,
+                                "parameters": {
+                                    "option_one": _term_option_one,
+                                    "option_second": _term_option_second,
+                                },
+                            }
+                        }
+                    },
                 )
-
+                self.user_history.insert_one(
+                    {
+                        "user": _user,
+                        "pwd": _pwd,
+                        "term": {
+                            "version": latest_term,
+                            "date_accepted": _date_now,
+                            "parameters": {
+                                "option_one": _term_option_one,
+                                "option_second": _term_option_second,
+                            },
+                        },
+                    }
+                )
         # create a new token with the user id inside
         access_token = create_access_token(identity=str(user["_id"]))
         refresh_token = create_refresh_token(str(user["_id"]))
