6794354eab6bc3476d38594c1ad2df74b6e2a3d2
Zaion Arruda <Zaion.Arruda@ball.com>
2023-10-31 22:33:48 -0300

    f65:feat-included router report

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -2,6 +2,7 @@ from flask_restful import Api
 
 from flaskr.resources.gleba import GlebaResource
 from flaskr.resources.terms import TermsResource
+from flaskr.resources.report import ReportResource
 from flaskr.resources.token import TokenRefresherResource, TokenResource
 from flaskr.resources.user import UserResource
 
@@ -11,6 +12,7 @@ def config_app_routes(app):
 
     __setting_route_doc(GlebaResource, "/gleba", api)
     __setting_route_doc(UserResource, "/user", api)
+    __setting_route_doc(ReportResource, "/report", api)
     __setting_route_doc(TermsResource, "/terms", api)
     __setting_route_doc(TokenResource, "/token", api)
     __setting_route_doc(TokenRefresherResource, '/token/refresh', api)
