c38eca0031444b8f15172b87dce4982c3d42fb23
ZaionKun <gomeszaion@gmail.com>
2023-11-26 17:36:25 -0300

    fix return stemporal

--- flaskr/resources/stemporal.py
+++ flaskr/resources/stemporal.py
@@ -17,9 +17,9 @@ class StemporalResource(Resource):
         print(stemporal)
         format_stemporal = []
         for dado in stemporal:
-            datas = dado[0][1:-1].split(",")
-            NDVIReal = dado[1][1:-1].split(",")
-            Previsao = dado[2][1:-1].split(",")
+            datas = [datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%m-%d") for date in dado[0][1:-1].split(',')]  # Convertendo para strings
+            NDVIReal = [float(value) for value in dado[1][1:-1].split(',')]  # Convertendo para floats
+            Previsao = [float(value) for value in dado[2][1:-1].split(',')]  # Convertendo para floats
 
             previsao_dict = {
                 "data_teste": datas,
